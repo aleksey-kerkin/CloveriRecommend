258d33ee001f6a078f7eae3c4a8e8db51268b81d
Aleksey Kerkin <aleksey.kerkin@yandex.ru>
2024-11-13 16:46:29 +0500

    add example model && fastapi logic

--- app/app.py
+++ app/app.py
@@ -1,8 +1,36 @@
+from typing import Dict
+
 from fastapi import FastAPI
+from models import RecommendationSystem
+
+from ml.model import load_model, predict  # Предположим, что у нас есть такие функции
 
 app = FastAPI()
+model = None
 
 
+# Создаем маршрут
 @app.get("/")
 def index():
-    return {"text": "Recommendation system"}
+    return {"text": "Система рекомендаций товаров"}
+
+
+# Регистрируем функцию, которая будет выполняться при старте
+@app.on_event("startup")
+def startup_event():
+    global model
+    model = load_model()
+
+
+# Обработчики маршрутов FastAPI
+@app.get("/predict", response_model=RecommendationSystem)
+def predict_recommendations(entity_id: int, entity_type: str, features: Dict[str, float]):
+    recommendations = predict(model, entity_id, entity_type, features)
+
+    response = RecommendationSystem(
+        entity_id=entity_id,
+        entity_type=entity_type,
+        features=features,
+        recommendations=recommendations,
+    )
+    return response
---
+++ app/models.py
@@ -0,0 +1,23 @@
+from typing import Dict, List, Union
+
+from pydantic import BaseModel
+
+
+class RecommendationSystem(BaseModel):
+    entity_id: int  # Идентификатор пользователя или товара
+    entity_type: str  # 'user' или 'product'
+    features: Dict[str, float]  # Хар-ки пользователя или товара
+    recommendations: List[Dict[str, Union[int, float]]]  # Список рекомендаций с ID и оценками
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "entity_id": 123,
+                "entity_type": "user",
+                "features": {"age": 25, "gender": 0.0, "interest_score": 0.8},
+                "recommendations": [
+                    {"product_id": 456, "recommendation_score": 0.9},
+                    {"product_id": 789, "recommendation_score": 0.85},
+                ],
+            }
+        }
